b92c8a40742a922361067f131e8ab099e3f4cfe6
Piyush Goyal <52343801+piyushgoyal1620@users.noreply.github.com>
2020-10-01 00:23:01 +0530

    Create counter2.py

---
+++ counter2.py
@@ -0,0 +1,8 @@
+import collections  # import collection here
+
+c = collections.Counter()
+print('intial:', c)
+c.update('abcdaab')
+print('sequence:', c)
+c.update({'a': 1, 'd': 5, 'f': 10})
+print("Dict:", c)
